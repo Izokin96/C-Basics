810b30bda6939345aeb03e48f2bd8aa3dc629945
Isaac Chávez <isaac_chavez_96@hotmail.com>
2024-10-10 10:07:34 -0700

    Uploading new basic exercise
    This is the first day I will upload more c# code for practice.

---
+++ A1- Name and sum of numbers.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+ // Compiler version 4.0, .NET Framework 4.5
+
+
+ namespace Dcoder
+ {
+   public class Program
+   {
+     public static void Main(string[] args)
+     {
+       Console.WriteLine("||-----------------------------------||");
+       Console.WriteLine("||            Excercice 1            ||");
+       Console.WriteLine("||       Name and sum of numbers     ||");
+       Console.WriteLine("||-----------------------------------||");
+       int num1 = 2;
+       int num2 = 1;
+       int res = num1+num2;
+       Console.WriteLine("Hello User! the sum of the two numbers that you stated is: "+ res);
+       
+     }
+   }
+ }
